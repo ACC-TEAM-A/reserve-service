a34c1e9b81119d7875b2508b0c2f11d48abe20a9
eojin0814 <leeeojin6@gmail.com>
2024-03-12 00:42:20 +0900

    feat/init

---
+++ src/main/java/com/eureka/reserveserivce/ReserveserivceApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.reserveserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveserivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveserivceApplication.class, args);
+    }
+
+}
