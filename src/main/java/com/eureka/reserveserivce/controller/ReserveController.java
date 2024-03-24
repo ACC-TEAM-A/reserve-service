ed16fdf319669e55696fb58a6efc06a9d0d7211b
aeeazip <aeea0520@gmail.com>
2024-03-24 19:04:20 +0900

    fix : 메소드 mapping

--- src/main/java/com/eureka/reserveserivce/controller/ReserveController.java
+++ src/main/java/com/eureka/reserveserivce/controller/ReserveController.java
@@ -4,21 +4,25 @@ import com.eureka.reserveserivce.domain.Reserve;
 import com.eureka.reserveserivce.service.ReserveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/reserve")
 @RequiredArgsConstructor
 public class ReserveController {
     private final ReserveService reserveService;
 
     // 예약 생성
+    @PostMapping("")
     public ResponseEntity<Reserve> createReserve() {
         return ResponseEntity.ok(reserveService.createReserve());
     }
 
     // 예약 조회
+    @GetMapping("")
     public ResponseEntity<List<Reserve>> getReserves() {
         return ResponseEntity.ok(reserveService.getReserves());
     }
